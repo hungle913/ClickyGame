9a01ae77bd87719d01ed866cd18f487e1ce67a77
Hung Le <hungle913@gmail.com>
2020-06-05 09:39:40 -0700

    Updates

---
+++ precache-manifest.0a68a7c9adef17ad39c601a08fc7041c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbb51abf5fc8339237e395bf6e8e83d",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "c7f897ebeeed71dd0925",
+    "url": "/ClickyGame/static/css/main.bbb55017.chunk.css"
+  },
+  {
+    "revision": "2bd0f7eaf8daae6a35fc",
+    "url": "/ClickyGame/static/js/2.c581d9da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.c581d9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f897ebeeed71dd0925",
+    "url": "/ClickyGame/static/js/main.ac94742c.chunk.js"
+  },
+  {
+    "revision": "52ba01ea425dc5e7e4bc",
+    "url": "/ClickyGame/static/js/runtime-main.33951b4a.js"
+  },
+  {
+    "revision": "5d868d4505c73b237321d4c344ec8539",
+    "url": "/ClickyGame/static/media/PumkinBG.5d868d45.jpg"
+  }
+]);
