223f797af425c1878a6c3169d42e3bb667d63022
Hung Le <hungle913@gmail.com>
2020-06-05 11:27:52 -0700

    changed alt image to reflect new alt url

--- src/components/FaceCard/index.js
+++ src/components/FaceCard/index.js
@@ -5,7 +5,7 @@ function FaceCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} id={props.id} onClick={props.onClick} src={props.image} clicked={props.isClicked} />
+        <img alt={props.alt} id={props.id} onClick={props.onClick} src={props.image} clicked={props.isClicked} />
       </div>
     </div>
   );
